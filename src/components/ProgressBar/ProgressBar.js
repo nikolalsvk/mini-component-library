eebfc8912b1cf5df1f2bab8fdd359a378420c286
Nikola Đuza <nikolaseap@gmail.com>
2021-06-11 15:58:14 +0200

    Create ProgressBar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,65 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const barStyles = {
+    large: {
+      "--line-height": "16px",
+      "--radius": "8px",
+      "--pading": "4px",
+    },
+    medium: {
+      "--line-height": "12px",
+      "--radius": "4px",
+      "--pading": 0,
+    },
+    small: {
+      "--line-height": "8px",
+      "--radius": "4px",
+      "--pading": 0,
+    },
+  };
+
+  const style = barStyles[size];
+
+  return (
+    <BarBase
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={style}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <BarLine width={value} />
+      </BarWrapper>
+    </BarBase>
+  );
 };
+
+const BarBase = styled.div`
+  background: hsla(0, 0%, 50%, 0.15);
+  box-shadow: 0px 2px 4px 0px hsla(0, 0%, 50%, 0.35) inset;
+
+  border-radius: var(--radius);
+  padding: var(--pading);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const BarLine = styled.div`
+  height: var(--line-height);
+  width: ${(prop) => prop.width}%;
+  background: #4747eb;
+  border-radius: 4px 0px 0px 4px;
+`;
 
 export default ProgressBar;
